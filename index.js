d6658343a009b2df9d81b41107d83be7ee6a06de
melind <melinda93@hotmail.fr>
2018-05-21 14:27:27 +0200

    r

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+$( document ).ready((function){
+$('.carousel').carousel();
+$('[data-toggle="tooltip"]').tooltip();
+});
